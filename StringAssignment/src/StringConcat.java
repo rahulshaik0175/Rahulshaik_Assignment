1dca469f8bdfe7297256a64a32632a6249ba6b59
rahulshaik0175 <rahulshaik0175@gmail.com>
2021-11-09 22:22:46 +0530

    File is Committed

---
+++ StringAssignment/src/StringConcat.java
@@ -0,0 +1,9 @@
+ public class StringConcat{
+	 public static void main(String[] args) {
+		String str1 = "Hello";
+		String str2 = " How are you?";
+		String str3 = str1.concat(str2);
+		System.out.println("The concatenated string is: "+str3);
+	}
+ }
+
---
+++ StringAssignment/src/StringLength.java
@@ -0,0 +1,9 @@
+public class StringLength
+{
+public static void main(String[] args) 
+{
+	String str1 = "Hello World";
+	int size = str1.length();
+	System.out.println(size);
+	}
+}
