709b437454a46fb45afa4e549f632bbb99f976b2
rahulshaik0175 <rahulshaik0175@gmail.com>
2021-11-17 23:07:29 +0530

    MVC Assignments

---
+++ SpringMVCAssignments/src/main/resources/com/rahul/SpringMVCAssignments/HomeController.java
@@ -0,0 +1,17 @@
+package com.rahul.SpringMVCAssignments;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/index")
+	public String home()
+	{
+
+		System.out.println("This is index page and has a hyperlink once u click on hyperlink it will redirect to Helloworld controller");
+
+		return "index";
+	}
+}
---
+++ SpringMVCAssignments/src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Index Page</title>
+</head>
+<body>
+<h1 style="color:Tomato;">Hello Everyone!!!!</h1>
+<h2 style="color:DodgerBlue;"><b><i>I'm Rahul</b></i></h2>
+<a class="opcion"  href="<%= request.getContextPath()%>/helloworld">ClickHereForHelloWorldController</a>
+
+</body>
+</html>
